436e216f2a3c92e220a72776de9ef93c3c78cd21
gdmar <38363433+gdmar@users.noreply.github.com>
2023-04-14 18:23:53 -0300

    Update Principal.java

--- src/ar/edu/unju/fi/ejercicio01/Principal.java
+++ src/ar/edu/unju/fi/ejercicio01/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
     public static void main(String[] args) {
         String alumno1 = "Farfán Sebastian";
         String alumno2 = "otro integrante";
-        String alumno3 = "otro integrante";
+        String alumno3 = "Silisque Maria";
         String alumno4 = "otro integrante";
 
         System.out.println("Bienvenido a Programación Visual: " + alumno1 + ", " + alumno2 + ", " + alumno3 + ", " + alumno4);
